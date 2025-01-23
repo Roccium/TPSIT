936ea44387ab2fabd33ef3a391a2f3a5d5a2f937
rocco.marani <rocco.maranitassinari@itiszuccante.edu.it>
2025-01-23 13:43:18 +0100

    test
    Signed-off-by: rocco.marani <rocco.maranitassinari@itiszuccante.edu.it>

--- src/player.java
+++ src/player.java
@@ -2,29 +2,29 @@ import java.util.Random;
 import java.util.concurrent.Semaphore;
 
 public class player implements Runnable {
-    Semaphore s;
+    Semaphore sema1;
+    Semaphore sema2;
     int sign;
-    boolean pareggio=true;
     int[][]array;
     Random random = new Random();
-    public player(Semaphore z,int[][]p,int L){
-        this.s=z;
+    public player(Semaphore za,Semaphore altro,int[][]p,int L){
+        this.sema1=za;
+        this.sema2=altro;
         this.sign=L;
         this.array=p;
     }
     int i;
     @Override
-    public void run()
-    {
-        while (!checkvittoria()){
-            try {
-                s.acquire();
-                Thread.sleep(random.nextInt(500)+500);
-                if (!pareggio){
-                    System.out.println("\n"+"pareggio");
+    public void run(){
+        while (true){
+            if (checkvittoriaepareggio()==0) {
+                try {
 
-                }
-                else {
+                    if (sema1.tryAcquire()){
+                        sema2.acquire();
+                        sema1.acquire();
+                    }
+                    Thread.sleep(random.nextInt(500)+500);
                     while (true){
                         int a = random.nextInt(3);
                         int b = random.nextInt(3);
@@ -41,50 +41,67 @@ public class player implements Runnable {
                             System.out.println("\n");
                             break;
                         }
+
                     }
                 }
-
-                if (checkvittoria()){
-                    System.out.println(sign+" vince");
+                catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    if (checkvittoriaepareggio()==0){
+                        sema1.release();
+                        sema2.release();
+                    }
+                    else {
+                        System.out.println("la partita è finita");
+                        return;
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
                 }
-
-
             }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            finally {
-                s.release();
-            }
-        }
-
-        }
-        public boolean checkpareggio(){
-            for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 3; j++) {
-                    pareggio=(array[i][j]==0)&&pareggio;
+            else{
+                if (checkvittoriaepareggio()==1) {
+                    System.out.println("ha vinto il giocatore "+sign);
+                    break;
+                }
+                else{
+                    System.out.println("pareggio");
+                    break;
                 }
             }
-            return pareggio;
         }
-        public boolean checkvittoria() {
-            for (int j = 0; j < 3; j++) {
-                if (this.array[j][0]==this.sign&&this.array[j][1]==this.sign&&this.array[j][2]==this.sign){
-                    return true;
-                }
-                if (this.array[0][j]==this.sign&&this.array[1][j]==this.sign&&this.array[2][j]==this.sign){
-                    return true;
-                }
-            }
-            if (this.array[0][0]==this.sign&&this.array[1][1]==this.sign&&this.array[2][2]==this.sign){
-                return true;
-            }
-            if (this.array[0][2]==this.sign&&this.array[1][1]==this.sign&&this.array[2][0]==this.sign){
-                return true;
-            }
-            return false;
-        }
-
     }
 
+    public int checkvittoriaepareggio() {
+        int pareggio=1;
+        for (int j = 0; j < 3; j++) {
+            if (this.array[j][0]==this.sign&&this.array[j][1]==this.sign&&this.array[j][2]==this.sign){
+                return 1;
+            }
+            if (this.array[0][j]==this.sign&&this.array[1][j]==this.sign&&this.array[2][j]==this.sign){
+                return 1;
+            }
+        }
+        if (this.array[0][0]==this.sign&&this.array[1][1]==this.sign&&this.array[2][2]==this.sign){
+            return 1;
+        }
+        if (this.array[0][2]==this.sign&&this.array[1][1]==this.sign&&this.array[2][0]==this.sign){
+            return 1;
+        }
+        for (int k = 0; k < array.length; k++) {
+            for (int j = 0; j < array.length; j++) {
+                pareggio=array[k][j]*pareggio;
+            }
+        }
+        if(pareggio!=0){
+            return 2;
+        }
+        return 0;
+    }
+
+}

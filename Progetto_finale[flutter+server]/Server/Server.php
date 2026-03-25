d51b1cc920ca8d4c3845c2b417c47c3056d03d09
Marani Tassinari Rocco <rocco.marani@cz.lan>
2026-03-25 12:12:09 +0100

    update 1.1

--- Progetto_finale[flutter+server]/Server/Server.php
+++ Progetto_finale[flutter+server]/Server/Server.php
@@ -1,6 +1,21 @@
 <?php 
-echo "42"
-
+$messaggio = json_decode("",true);
+$nome = "";
+$password="";
+if (isset($messaggio['nome']) && isset($messaggio['password'])) {
+$nome = $messaggio['nome']
+$password = $messaggio['password']
+}
+//nel json avro tipo di messaggio e lo estraggo e faccio le chiamate adeguate
+switch ($variable) {
+    case 'value':
+        # code...
+        break;
+    
+    default:
+        # code...
+        break;
+}
 function registrazione($none,$password){
     if (/* se risultato della query x nome esiste gia */) {
         return "un account con questo nome esiste già";
@@ -10,6 +25,7 @@ function registrazione($none,$password){
     }
 }
 function autenticazione($none,$password){
+        //query x nome per vedere se esiste l'utente
         //query x $nome e password
         if (/*se la query va in errore */) {
             return "errore";

c7e39a4ea4fcc9e21608968cfff964580735ac48
Rocco <rocco.mt@icloud.com>
2026-03-26 17:20:27 +0100

    tutto appost

--- Progetto_finale[flutter+server]/progetto_finale/lib/notiefier.dart
+++ Progetto_finale[flutter+server]/progetto_finale/lib/notiefier.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:progetto_finale/models.dart';
 
 class ArmadioListNotifier with ChangeNotifier {
-
-
+  final container = <Contenitore>[];
   void addContenitore(){
 
   }
---
+++ Progetto_finale[flutter+server]/progetto_finale/lib/widgets.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:progetto_finale/models.dart';
+
+class Contenitore extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Row();//da vedere cosa mettere
+  }
+}
+
+class TodoItem extends StatelessWidget {
+  const TodoItem({
+    required this.immagine,
+    required this.onDelete,
+    required this.onToggle,
+    super.key,
+  });
+
+  final Immagine immagine;
+  final VoidCallback onDelete;
+  final VoidCallback onToggle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image(image: FileImage(File('/path/to/file.jpg')));
+  }
+}

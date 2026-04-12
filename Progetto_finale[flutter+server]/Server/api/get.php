adbd65994abff263f5bfb52d2cd69d3ad9b3a928
Rocco <rocco.mt@icloud.com>
2026-04-13 00:21:25 +0200

    un po' di php

--- Progetto_finale[flutter+server]/Server/api/get.php
+++ Progetto_finale[flutter+server]/Server/api/get.php
@@ -10,7 +10,9 @@ if (isset($messaggio['nome']) && isset($messaggio['password'])) {
     $nome = $messaggio['nome'];      
     $password = $messaggio['password']; 
 }
-
+if (isset($messaggio['id_fotos'])) {
+    $fotos = $messaggio['id_fotos'];
+}
 $azione = $messaggio['azione'] ?? '';
 
 switch ($azione) {
@@ -22,6 +24,9 @@ switch ($azione) {
         echo json_encode(["message" => autenticazione($nome, $password)]);
         break;
 
+    case 'primaconessione':
+        echo json_encode(["message" => primaconessione($nome, $fotos)]);
+        break;
     default:
         http_response_code(400);
         echo json_encode(["error" => "Azione non riconosciuta"]);
@@ -35,5 +40,21 @@ function registrazione($nom,$pass){
     $query = $mysqli->prepare("INSERT INTO utenti VALUES ( ?, ?); ;");
     $query->bind_param('ss',$nom,$pass);
 }
-
+function primaconessione($nom,$listaJson){
+        $query = $mysqli->prepare("SELECT id_foto FROM `foto` INNER JOIN utenti ON foto.id_utente = utenti.ID WHERE utenti.nome = ?");
+        $query->bind_param('s',$nom,$pass);
+        $result=$query->execute();
+        $listaSQL = [];
+        while ($row = $result->fetch_assoc()) {
+            $listaSQL[] = $row['link'];
+        }
+        $soloInJson = array_diff($listaJson, $listaSQL);
+        if (empty($soloInJson)) {
+            return "tutto aggiornato";
+        }
+        else{
+            //devi andarti a prendere le immagine inserirle in json e mandarle
+        }
+        
+}
 ?>
